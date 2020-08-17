40889c8cda416bb2edd311e0854301c22e0e3510
Al Butcher <alastair.butcher@grabba.com>
2020-08-18 08:24:06 +1000

    Initial Commit

---
+++ ClearData/ClearData/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using ClearData.Models;
+using ClearData.ViewModels;
+
+namespace ClearData.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
