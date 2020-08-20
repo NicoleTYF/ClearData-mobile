7cc3db0dbd5863df4cacb42035a8d426cde4fc02
lukekamols <luke.kamols1@gmail.com>
2020-08-20 20:06:32 +1000

    Made a new page for the data management

---
+++ ClearData/ClearData/ViewModels/ManageDataViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace ClearData.ViewModels
+{
+    public class ManageDataViewModel : BaseViewModel
+    {
+        public ManageDataViewModel()
+        {
+            Title = "Manage Data";
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamain-quickstart"));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
---
+++ ClearData/ClearData/Views/ManageDataPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ClearData.Views
+{
+    public partial class ManageDataPage : ContentPage
+    {
+        public ManageDataPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
