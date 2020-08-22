62ac3503ce589d1fc22c903dfd4a52518d912e40
lukekamols <luke.kamols1@gmail.com>
2020-08-22 17:54:19 +1000

    little commit to try and merge with master

--- ClearData/ClearData/ViewModels/ManageDataViewModel.cs
+++ ClearData/ClearData/ViewModels/ManageDataViewModel.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using ClearData.Models;
+using ClearData.Services;
+using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Essentials;
 using Xamarin.Forms;
@@ -7,10 +11,25 @@ namespace ClearData.ViewModels
 {
     public class ManageDataViewModel : BaseViewModel
     {
+        public ObservableCollection<DataType> DataTypes { get; }
+        public Command LoadDataTypesCommand { get; }
         public ManageDataViewModel()
         {
             Title = "Manage Data";
-            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamain-quickstart"));
+            DataTypes = new ObservableCollection<DataType>();
+
+        }
+
+        async Task ExecuteLoadDataTypesCommand()
+        {
+            IsBusy = true;
+
+            try
+            {
+                DataTypes.Clear();
+                var dataTypes = await PermissionsDataStore.GetDataTypesAsync(true);
+                //UP TO HERE
+            }
         }
 
         public ICommand OpenWebCommand { get; }
