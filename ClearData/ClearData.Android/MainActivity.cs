897aeec003a75e77bba23742cabad8b06aee0a10
NicoleTY <tszyannicole.fung@uq.net.au>
2020-08-27 09:04:41 +1000

    Finishing up custom deisgn for username and pswd login entries

--- ClearData/ClearData.Android/MainActivity.cs
+++ ClearData/ClearData.Android/MainActivity.cs
@@ -23,10 +23,16 @@ namespace ClearData.Droid
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
+        /**
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        */
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
---
+++ ClearData/ClearData.Android/Resources/LoginEntryRenderer.cs
@@ -0,0 +1,32 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Android.Graphics.Drawables;
+using ClearData.Controls;
+using ClearData.Android;
+using Android.Content;
+
+[assembly: ExportRenderer(typeof(LoginEntry), typeof(LoginEntryRenderer))]
+namespace ClearData.Android
+{
+    public class LoginEntryRenderer : EntryRenderer
+    {
+        public LoginEntryRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.SetBackgroundColor(global::Android.Graphics.Color.Transparent);
+                Control.SetTextSize(global::Android.Util.ComplexUnitType.Dip, 20);
+                Control.SetHighlightColor(global::Android.Graphics.Color.ParseColor("#FFE88F")); // amber color
+                Control.SetWidth(230);
+                Control.SetHeight(50);
+                Control.SetTextColor(global::Android.Graphics.Color.White);
+            }
+        }
+    }
+}
