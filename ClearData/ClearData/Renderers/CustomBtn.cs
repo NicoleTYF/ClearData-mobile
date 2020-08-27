9f7a0155c763896973d08ce341cc58edb84f0051
NicoleTY <tszyannicole.fung@uq.net.au>
2020-08-28 06:54:53 +1000

    Try to create custom button for Data Types and Service

---
+++ ClearData/ClearData/Renderers/CustomBtn.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ClearData.Controls
+{
+    public class CustomBtn : Button
+    {
+        public static readonly BindableProperty PaddingProperty = BindableProperty.Create(nameof(Padding), typeof(Thickness), typeof(Button), default(Thickness));
+
+        public Thickness Padding {
+            get => (Thickness)this.GetValue(PaddingProperty);
+            set => this.SetValue(PaddingProperty, value);
+        }
+    }
+}
