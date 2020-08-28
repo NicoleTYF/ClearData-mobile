cce51b3c7a133473208501d35ece73df6874935a
NicoleTY <tszyannicole.fung@uq.net.au>
2020-08-29 02:02:32 +1000

    Created String Case Converter

---
+++ ClearData/ClearData/Converters/StringCaseConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace ClearData.Converters
+{
+    public class StringCaseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string param = System.Convert.ToString(parameter) ?? "u";
+
+            switch (param.ToUpper())
+            {
+                case "U":
+                    return ((string)value).ToUpper();
+                case "L":
+                    return ((string)value).ToLower();
+                default:
+                    return ((string)value);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string param = System.Convert.ToString(parameter) ?? "U";
+
+            switch (param.ToUpperInvariant())
+            {
+                case "U":
+                    return ((string)value).ToLower();
+                case "L":
+                    return ((string)value).ToUpper();
+                default:
+                    return ((string)value);
+            }
+        }
+    }
+}
