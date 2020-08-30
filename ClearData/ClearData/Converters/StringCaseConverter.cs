4a3f909168620136c2380ff7de947caf094fca08
NicoleTY <tszyannicole.fung@uq.net.au>
2020-08-31 03:37:19 +1000

    Fix layout issues.

--- ClearData/ClearData/Converters/StringCaseConverter.cs
+++ ClearData/ClearData/Converters/StringCaseConverter.cs
@@ -9,32 +9,12 @@ namespace ClearData.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            string param = System.Convert.ToString(parameter) ?? "u";
-
-            switch (param.ToUpper())
-            {
-                case "U":
-                    return ((string)value).ToUpper();
-                case "L":
-                    return ((string)value).ToLower();
-                default:
-                    return ((string)value);
-            }
+            return ((string)value).ToUpper();
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            string param = System.Convert.ToString(parameter) ?? "U";
-
-            switch (param.ToUpperInvariant())
-            {
-                case "U":
-                    return ((string)value).ToLower();
-                case "L":
-                    return ((string)value).ToUpper();
-                default:
-                    return ((string)value);
-            }
+            return ((string)value).ToLower();
         }
     }
 }
