7d1074a02355f9aa1212f0770ed95ae67ba30bb4
Al Butcher <alastair.butcher@grabba.com>
2020-09-01 09:07:36 +1000

    Fixed linking and binding for signup page

--- ClearData/ClearData/ViewModels/SignupViewModel.cs
+++ ClearData/ClearData/ViewModels/SignupViewModel.cs
@@ -1,10 +1,54 @@
 ﻿using System;
+using ClearData.Views;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace ClearData.ViewModels
 {
-    public class SignupViewModel
+    public class SignupViewModel : BaseViewModel
     {
+        public Command CreateAccCom { get; }
+
+        private string uText;
+        public string UsernameText
+        {
+            get => uText;
+            set => SetProperty(ref uText, value);
+        }
+
+        private DateTime dob;
+        public DateTime DOB
+        {
+            get => dob;
+            set => SetProperty(ref dob, value);
+        }
+
+        private string place;
+        public string Birthplace
+        {
+            get => place;
+            set => SetProperty(ref place, value);
+        }
+
+        public SignupViewModel() 
+        {
+            CreateAccCom = new Command(createAccount);
+        }
+
+        private async void createAccount()
+        {
+            if (Birthplace == null || UsernameText == null || DOB.Year == 1900)
+            {
+                // Fail
+                await Application.Current.MainPage.DisplayAlert("Alert", "BEEEEEEAAAANSSSSS", "Okay..?");
+            }
+            else
+            {
+                // Success
+                await Application.Current.MainPage.DisplayAlert("Alert", "Well done, you made it", "Hooray!");
+            }
+        }
+
     }
 }
