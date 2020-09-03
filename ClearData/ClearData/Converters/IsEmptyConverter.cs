43004ac3e44b72b9c60edcf3e85c2cc06eaed97b
lukekamols <luke.kamols1@gmail.com>
2020-09-03 18:45:18 +1000

    added a 'nothing to display' on empty log lists

---
+++ ClearData/ClearData/Converters/IsEmptyConverter.cs
@@ -0,0 +1,25 @@
+﻿using ClearData.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace ClearData.Converters
+{
+    public class IsEmptyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //the goal here is to return true if the supplied list is empty
+            ObservableCollection<Log> logs = (ObservableCollection<Log>)value;
+            return logs.Count == 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //this conversion shouldn't ever be performed, may not be universally correct but shouldn't matter
+            return new IndexedLogCollection();
+        }
+    }
+}
