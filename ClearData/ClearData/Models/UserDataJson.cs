971e2ceb07f93a401a074f30f80b6071a938d07a
Al Butcher <alastair.butcher@grabba.com>
2020-09-05 21:11:27 +1000

    Initial commit of REST API code

---
+++ ClearData/ClearData/Models/UserDataJson.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClearData.Models
+{
+    public class UserDataJson
+    {
+        public string username { get; set; }
+
+        public DateTime dob { get; set;}
+
+        public string birthplace { get; set; }
+    }
+}
